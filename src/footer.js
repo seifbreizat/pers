40e2873de617c6fc44d22636b36d1e1fd1131b78
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-03 02:21:20 +0300

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -10,13 +10,13 @@ const Footer =()=> {
       <p>Seif @ {new Date().getFullYear()}.All Rights reserved</p>         
    <div>
       <a href="#">
-            <img class="link-img" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-facebook.png"/>
+            <img class="link-img" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-facebook.png" alt="instegram"/>
           </a>
           <a href="#">
-            <img class="link-img" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-twitter.png"/>
+            <img class="link-img" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-twitter.png" alt="instegram"/>
           </a>
           <a href="#">
-            <img class="link-img" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-instegram.png"/>
+            <img class="link-img" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-instegram.png" alt="instegram"/>
           </a>
           
           </div>
