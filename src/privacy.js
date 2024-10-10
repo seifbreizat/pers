16d41f4e32e85429fabc0c852fc3985850a381f1
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-10 05:19:04 +0300

    Update privacy.js

--- src/privacy.js
+++ src/privacy.js
@@ -3,6 +3,39 @@ import "./App.css"
 function Privacy () = {
 
   return (
-    <p>  </p>
+    <p> Privacy Policy
+Information Collection:
+
+We collect personal information such as name, email address, and browsing data.
+Information is collected directly through forms and indirectly through cookies, web beacons, and third-party services.
+Use of Information:
+
+Personal data is used to enhance user experience, personalize ads, and communicate updates.
+Google and third-party vendors use cookies to serve ads based on prior visits to our site.
+Google Ads and Third-party Advertising:
+
+Our site uses Google Ads remarketing to advertise online.
+Google and third-party vendors use cookies to show ads based on your browsing activity.
+Google Analytics collects visitor data to improve services and personalize ads.
+Cookie Consent and Opt-out:
+
+By using our site, you agree to the use of cookies for personalized advertising.
+Users can opt out of Google’s use of cookies by visiting Google Ads Settings.
+You may opt-out of third-party vendors’ cookies by visiting the Network Advertising Initiative opt-out page.
+Third-party Links and Policies:
+
+We may display ads from third-party services, and those services may have their own privacy policies.
+Users are encouraged to review third-party privacy policies.
+Data Security:
+
+We prioritize the security and privacy of your data and implement appropriate measures to protect it.
+Compliance with Laws:
+
+Our site complies with relevant privacy regulations, including the GDPR and CCPA.
+Users have the right to access, modify, or request deletion of their personal information.
+Policy Updates:
+
+This policy may be updated periodically. The latest update will be reflected with the date of modification. </p>
     );
 };
+export default Privacy;
