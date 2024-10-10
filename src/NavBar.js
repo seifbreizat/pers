fc2774f2996fc15a9cdfed55ecbaac7756b49313
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-10 06:11:19 +0300

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Link} from "react-router-dom";
+import treeLogo "tree.svg";
 const NavBar = () => {
 
     return (
 
        
         <nav>
-             <img src="tree.svg" height={100} width={100} alt= "tree "/>
+          <img src={treeLogo} className="logo react" alt="React logo" />
+
                 <Link to="/"><h1>Home</h1></Link>
                 <Link to="/about"><h1>About</h1></Link>
                 <Link to="/BookConsult"><h1> Contact </h1></Link>
