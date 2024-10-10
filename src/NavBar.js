54cac2b5ecd255386a7364e58e9b63ef57aefae1
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-10 05:00:53 +0300

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
              <img src="tree.svg" height={100} width={100} alt= "tree "/>
                 <Link to="/"><h1>Home</h1></Link>
                 <Link to="/about"><h1>About</h1></Link>
-                <Link to="/BookConsult"><h1><span>  Book Consult </span> </h1></Link>
+                <Link to="/BookConsult"><h1> Contact </h1></Link>
 
               
         </nav>
