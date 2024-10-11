0cd54da573a60ccea9fb3cfdd39eba184f12b98d
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-11 05:34:11 +0300

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -7,13 +7,14 @@ const Footer =()=> {
     return(
       <>
         <footer>
-        <div>    
-        <a href="privacy"> Privacy </a>
-</div>
+       
       <div>
         
       <p>Seif @ {new Date().getFullYear()}.All Rights reserved</p>
     </div>
+     <div>    
+        <a href="privacy"> Privacy </a>
+</div>
    <div>
      
             <img class="link-img" src="https://workik-widget-assets.s3.amazonaws.com/Footer1-83/v1/images/Icon-facebook.png" alt="Facebook"/>
