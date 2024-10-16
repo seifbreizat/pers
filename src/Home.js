5ed0cafdf5574cf426d86eb87e51e8f9f1bcfef0
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-16 17:29:55 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,46 @@ const Home=()=> {
         <div> 
 
         
-          
+          <div class="template-1 css-17lldrv eb97p611"><h2 class="css-1dv1kvn">Highlights</h2>
+           <ol class="css-11jjg eb97p612"><li class="css-ikiut1"><article class="css-1nk4uew"><div class="story-body css-gjijuv">
+           <h3 class="css-j9v18q e27kk5e0"><a class="css-1u3p7j1" data-rref="" href="/2024/10/16/style/prada-axiom-space-suits.html">Space: The Final Fashion Frontier</a></h3>
+           <p class="css-f9p5k6 e27kk5e1">Prada and Axiom Space unveiled their NASA spacesuits, in the most far-out collaboration yet.</p>
+           <p class="css-ql2p4f e1hr934v3"><span class="css-me3p27"><span data-testid="todays-date" aria-label="6h ago">6h ago</span></span>
+           <span class="css-1ekjaje e1hr934v4"></span><span class="css-9voj2j">By<!-- --> <span class="css-1baulvz last-byline" itemprop="name">Vanessa Friedman</span></span></p></div><figure class="photo css-1ekd4wt e759ksq0" role="group"><div class="css-0 e1y9711p0"><img src="https://static01.nyt.com/images/2024/10/16/multimedia/16PRADA-SPACE-03-qpml/16PRADA-SPACE-03-qpml-jumbo.jpg?auto=webp" srcset="https://static01.nyt.com/images/2024/10/16/multimedia/16PRADA-SPACE-03-qpml/16PRADA-SPACE-03-qpml-jumbo.jpg?auto=webp 820w" sizes="(min-width: 1280px) 788px, (min-width: 765px) 64vw, 95vw" loading="auto" alt="The spacesuit designed by Axiom Space with Prada’s collaboration, which NASA will use for the Artemis III lunar mission." class="css-c1t3s7"></div>
+    <figcaption class="css-aqbich e759ksq1"><span class="css-mo7dgs e759ksq2">
+           <span class="css-1dv1kvn">Credit</span>Axiom, Prada</span></figcaption></figure></article></li>
+           <li class="css-ikiut1"><article class="css-2j26di"><div class="story-body css-10wtrbd">
+           <div class="css-10lk8ze e1hr934v0">ASK Vanessa</div><h3 class="css-1x50auk e27kk5e2">
+           <a class="css-1u3p7j1" data-rref="" href="/2024/10/14/style/leopard-cheetah-print-fashion.html">Will Leopard Print Ever Go Out of Style?</a></h3>
+           <p class="css-1s1laau e27kk5e3">A reader is puzzled by the popularity of animal print in women’s fashion. Our critic offers an explanation for its appeal.</p>
+           <p class="css-14apgjp e1hr934v3"><span class="css-me3p27"><span data-testid="todays-date" aria-label="2d ago">2d ago</span></span>
+           <span class="css-1ekjaje e1hr934v4"></span><span class="css-9voj2j">By<!-- --> <span class="css-1baulvz last-byline" itemprop="name">Vanessa Friedman</span></span></p></div>
+           <figure class="photo css-b86txt e759ksq0" role="group"><div class="css-0 e1y9711p0">
+           <img src="https://static01.nyt.com/images/2024/10/11/multimedia/11OpenThread-02-bpcv/11OpenThread-02-bpcv-mediumThreeByTwo440.jpg?auto=webp" srcset="https://static01.nyt.com/images/2024/10/11/multimedia/11OpenThread-02-bpcv/11OpenThread-02-bpcv-videoLarge.jpg?auto=webp 768w, https://static01.nyt.com/images/2024/10/11/multimedia/11OpenThread-02-bpcv/11OpenThread-02-bpcv-mediumThreeByTwo225.jpg?auto=webp 225w, https://static01.nyt.com/images/2024/10/11/multimedia/11OpenThread-02-bpcv/11OpenThread-02-bpcv-mediumThreeByTwo440.jpg?auto=webp 440w" sizes="(min-width: 1280px) 373px, (min-width: 765px) 32vw, 95vw" loading="auto" alt="Elizabeth Taylor, in 1962. " class="css-19ucebh"></div>
+           <figcaption class="css-aqbich e759ksq1"><span class="css-tkto30 e759ksq2"><span class="css-1dv1kvn">Credit</span>Keystone/Hulton Archive, via Getty Images</span></figcaption></figure></article></li><li class="css-ikiut1"><article class="css-2j26di"><div class="story-body css-10wtrbd">
+           <div class="css-10lk8ze e1hr934v0">Styles Q. and A.</div><h3 class="css-1x50auk e27kk5e2">
+           <a class="css-1u3p7j1" data-rref="" href="/2024/10/14/style/us-army-declassified-photos-fashion-army.html">
+           Formerly Classified Documents Get the Fine Art Treatment</a></h3><p class="css-1s1laau e27kk5e3">
+           Hundreds of declassified U.S. Army photos are featured in the new book “Fashion Army.”</p>
+           <p class="css-14apgjp e1hr934v3"><span class="css-me3p27"><span data-testid="todays-date" aria-label="19h ago">19h ago</span></span>
+           <span class="css-1ekjaje e1hr934v4"></span>
+           <span class="css-9voj2j">By<!-- -->
+           <span class="css-1baulvz last-byline" itemprop="name">Guy Trebay</span></span></p></div>
+           <figure class="photo css-b86txt e759ksq0" role="group"><div class="css-0 e1y9711p0">
+           <img src="https://static01.nyt.com/images/2024/10/14/fashion/14STY-fashion-army-promo1/14STY-fashion-army-promo1-mediumThreeByTwo440.jpg?auto=webp" srcset="https://static01.nyt.com/images/2024/10/14/fashion/14STY-fashion-army-promo1/14STY-fashion-army-promo1-videoLarge.jpg?auto=webp 768w, https://static01.nyt.com/images/2024/10/14/fashion/14STY-fashion-army-promo1/14STY-fashion-army-promo1-mediumThreeByTwo225.jpg?auto=webp 225w, https://static01.nyt.com/images/2024/10/14/fashion/14STY-fashion-army-promo1/14STY-fashion-army-promo1-mediumThreeByTwo440.jpg?auto=webp 440w" sizes="(min-width: 1280px) 373px, (min-width: 765px) 32vw, 95vw" loading="auto" alt="" class="css-19ucebh"></div>
+           <figcaption class="css-aqbich e759ksq1"><span class="css-tkto30 e759ksq2">
+           <span class="css-1dv1kvn">Credit</span>US Army Natick Labs Archive</span></figcaption></figure></article></li>
+           <li class="css-ikiut1"><article class="css-2j26di"><div class="story-body css-10wtrbd">
+           <h3 class="css-1x50auk e27kk5e2"><a class="css-1u3p7j1" data-rref="" href="/2024/10/11/style/kim-jones-fendi-dior-lvmh.html">
+           Kim Jones Leaving Fendi</a></h3>
+           <p class="css-1s1laau e27kk5e3">From fashion world rumor to reality, another industry shake-up.</p><p class="css-14apgjp e1hr934v3">
+           <span class="css-me3p27"><span data-testid="todays-date" aria-label="October 11, 2024">October 11, 2024</span></span>
+           <span class="css-1ekjaje e1hr934v4"></span><span class="css-9voj2j">By<!-- --> 
+           <span class="css-1baulvz last-byline" itemprop="name">Vanessa Friedman</span></span></p></div>
+           <figure class="photo css-b86txt e759ksq0" role="group"><div class="css-0 e1y9711p0">
+           <img src="https://static01.nyt.com/images/2024/10/13/multimedia/11FENDI-DESIGNER-NEWS-01-flzw/11FENDI-DESIGNER-NEWS-01-flzw-mediumThreeByTwo440.jpg?auto=webp" srcset="https://static01.nyt.com/images/2024/10/13/multimedia/11FENDI-DESIGNER-NEWS-01-flzw/11FENDI-DESIGNER-NEWS-01-flzw-videoLarge.jpg?auto=webp 768w, https://static01.nyt.com/images/2024/10/13/multimedia/11FENDI-DESIGNER-NEWS-01-flzw/11FENDI-DESIGNER-NEWS-01-flzw-mediumThreeByTwo225.jpg?auto=webp 225w, https://static01.nyt.com/images/2024/10/13/multimedia/11FENDI-DESIGNER-NEWS-01-flzw/11FENDI-DESIGNER-NEWS-01-flzw-mediumThreeByTwo440.jpg?auto=webp 440w" sizes="(min-width: 1280px) 373px, (min-width: 765px) 32vw, 95vw" loading="auto" alt="Kim Jones, at his Fendi couture show in 2023." class="css-19ucebh"></div>
+           <figcaption class="css-aqbich e759ksq1"><span class="css-tkto30 e759ksq2">
+           <span class="css-1dv1kvn">Credit</span>Anne-Christine Poujoulat/Agence France-Presse &amp;mdash; Getty Images</span></figcaption></figure></article></li></ol></div>
        
         </div>
        <div class="content">
