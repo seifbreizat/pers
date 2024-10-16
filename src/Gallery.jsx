039d8f9c13cb39256dae4b17220bd91fec0b4d91
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-16 18:10:59 +0300

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,14 +4,9 @@ const Gallery = () => {
 
   return(
 
-    <img src="https://www.vogue.com/article/photovogue-voices" style={
-      width:100
-      height:100} alt="pic of art"/>
-   <img src="  https://assets.vogue.com/photos/6464e31c135790a539abf01e/4:3/w_960,c_limit/cecilia%20sordi%20campos.jpg
-" style={
-      width:100
-      height:100} alt="pic of art"/>
-    }
+    <img src="https://www.vogue.com/article/photovogue-voices" width:100 height:100 alt="pic of art"/>
+   <img src="https://assets.vogue.com/photos/6464e31c135790a539abf01e/4:3/w_960,c_limit/cecilia%20sordi%20campos.jpg" width:100 height:100 alt="pic of art"/>
+    
   )
 }
 
