f6a99a5d7719d9ec3dac621347851dfcd386ad2b
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-18 23:36:33 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -9,33 +9,14 @@ const Home=()=> {
         
 
         
-       <div class="content">
-        <div className="card white">
-      <div className="card-header" class="title">
-        
-        <h3>Web Development</h3>
-        <img src="illu.jpeg"  height={100} width={100} alt="IMF website first-page"/>
-      </div>
-      <div className="card-body">
-        <p> Develop custom websites that are responsive</p>
-      </div>
-    </div>
    
-    <div>
-       
 
-    <h1>"Life is to work where you are fit to embrace peace and calme and mental growth"</h1>
-
-            <br>
-            </br>
-                                       <p style={ {textAlign:'center'} }>Seif Albreizat</p>
-               </div>
-<div class="text-main">
+<div class="text-main" style={{padding-right:800px; position:fixed;}}>
        <h2> <strong> Fashion </strong> </h2>
        
 
              <p>Fashion
-Many fashion experts say that fashion is a source of inspiration and beauty.  Fashion is a great cultural scope that reflects the culture of the different communities living in this world and those that died. It is segmented to be elegant and sharp point in terms of showing personality and charisma as well as Religious affiliations and religious associations and occasions. Fashion is nice to be called the point of change especially at social or political visits and at work to adhere to work policies and environment settings. 
+Many fashion experts say that fashion is a source of inspiration and beauty. Fashion is a great cultural scope that reflects the culture of the different communities living in this world and those that died. It is segmented to be elegant and sharp point in terms of showing personality and charisma as well as Religious affiliations and religious associations and occasions. Fashion is nice to be called the point of change especially at social or political visits and at work to adhere to work policies and environment settings. 
 
 
 Four points aspects that determine fashion which are as follows:
