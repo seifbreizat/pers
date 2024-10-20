c9366e95ae836bbbe0bf559daf1c81de9813a17f
Seif Albreizat <106906962+seifbreizat@users.noreply.github.com>
2024-10-20 13:24:26 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -18,43 +18,22 @@ const Home=()=> {
              <p>Fashion
 Many fashion experts say that fashion is a source of inspiration and beauty. Fashion is a great cultural scope that reflects the culture of the different communities living in this world and those that died. It is segmented to be elegant and sharp point in terms of showing personality and charisma as well as Religious affiliations and religious associations and occasions. Fashion is nice to be called the point of change especially at social or political visits and at work to adhere to work policies and environment settings. 
 
+       <ul>
 
 Four points aspects that determine fashion which are as follows:
-•	Size depending on the individual who puts the clothes on.
-•	Color according to preferences. 
-•	 Compatibility with shoes or formal foot wear. 
-•	Material.
+•<li>	Size depending on the individual who puts the clothes on.</li>
+•	<li> Color according to preferences. </li>
+•	<li> Compatibility with shoes or formal foot wear. </li>
+•	<li>Material.</li>
 
 
-
+<ul>
 Fashion is good and wearing cools clothes is nice. 
 
 
 </p>
 
-        <div class="articles-container">
-       <div className="article">
-       <div className="article-title">
-       <h2>  <a href="article1.js">Vice President Kamala Harris on Her Race to the Finish
-       </a></h2>
-                                        <figcaption>By Nathan Heller Photography by Annie Leibovitz 
-                                                         October 11, 2024  </figcaption>
-       </div>
-                                                                <div className="article-title">
-       <h2>  <a href="article1.js">The First Woman on the Moon Will Wear This Prada Spacesuit
-       </a></h2>
-                                        <figcaption>By Luke Leitch </figcaption>
-       </div><div className="article-title">
-       <h2>  <a href="article1.js">Trends
-As a Denimhead, I’m All In on Jean Bags
 
-                                                                </a></h2>
-                                        <figcaption>By Christian Allaire </figcaption>
-       </div>
-                                                             
-       </div>
-       
-       </div>
     
        </div>
       
